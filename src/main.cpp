fcfc103523bd4106cf02e4a6246e09b2f0c5e866
Sina Solaimanpour <sina@uga.edu>
2014-10-11 12:53:27 -0400

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Sina Solaimanpour (sina@uga.edu)
+// Version     : 0.1
+// Description : UAV Reconnaissance problem for the programming assignment 2
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+enum actions {NORTH = 0, WEST = 1, SOUTH = 2, EAST = 3};
+
+
+
+int main() {
+
+
+
+	return 0;
+}
